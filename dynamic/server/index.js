e84074d392a7ca8cd31ff78231b348b37e90005e
iromashko <iromashko@me.com>
2021-02-02 01:58:52 +0300

    part 3

--- dynamic/server/index.js
+++ dynamic/server/index.js
@@ -14,3 +14,20 @@ const greetProtoDefinition = protoLoader.loadSync(greetProtoPath, {
 const greetPackageDefinition = grpc.loadPackageDefinition(
   greetProtoDefinition
 ).greet;
+
+function greet(call, callback) {
+  let firstName = call.request.greeting.first_name;
+  let lastName = call.request.greeting.last_name;
+
+  callback(null, { result: `Hello ${firstName} ${lastName}` });
+}
+
+function main() {
+  const server = new grpc.Server();
+  server.addService(greetPackageDefinition.GreetService.service, { greet });
+  server.bind('127.0.0.1:50051', grpc.ServerCredentials.createInsecure());
+  server.start();
+  console.log(`Server Running at http://localhost:50051`);
+}
+
+main();
